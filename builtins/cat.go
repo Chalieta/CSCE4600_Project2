e630cdf69647a80310986c70a4dfa969a4b00973
Leif Messinger <leifmessinger@gmail.com>
2023-05-10 17:10:57 -0500

    Added cat.go. Untested

---
+++ builtins/cat.go
@@ -0,0 +1,38 @@
+//Sussie Manovapillai
+//Description: Implementing the ls command.
+//4-18-2023 CSCE4600
+
+package builtins
+
+import (
+	"bufio"	//scanner
+	"fmt"
+	"log"
+	"os"
+)
+
+func ConcatenateFiles(args ...string) error{
+	if len(args) == 0{
+		//Redirect user input into user output until ctrlD
+		//That means I gotta handle the EOF signal
+
+		//Cat program done here https://stackoverflow.com/a/45914294/10141528
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		}
+		return scanner.Err()
+	}else{
+		//Concatenate all files in args
+		for i := 0; i < len(args); i++ {
+			data,err := os.ReadFile(args[i])
+			//From documentation https://pkg.go.dev/os#ReadFile
+			os.Stdout.Write(data)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		return nil
+	}
+	return nil	//Shouldn't get here in theory
+}
--- main.go
+++ main.go
@@ -86,6 +86,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return nil
 	case "ls":
 		return builtins.ListFilesDirectory(args...)
+	case "ls":
+		return builtins.ConcatenateFiles(args...)
 	case "open":
 		return builtins.OpenFileDirectory(args...)
 	}
